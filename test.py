db01184ec3c3c8d904b646f3b05695f6de169ba3
ZRunner <z.runner.mc@gmail.com>
2023-03-13 14:24:27 -0400

    fix test script

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
+import time
 from json import dumps
 
 from gatherer import Gatherer
+
 
 def main():
     gatherer = Gatherer()
     try:
         while True:
             print(dumps(gatherer.collect(), indent=2))
+            time.sleep(1)
     except KeyboardInterrupt:
         print("Closing script")
 

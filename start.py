1db258d3f341c33ddc4c2ff0d7758ff3accda847
ZRunner <z.runner.mc@gmail.com>
2023-03-14 19:52:55 -0400

    Revert "test log"
    This reverts commit 1d0169b23452b7af2ef9995f451b048a9d7464b6.

--- start.py
+++ start.py
@@ -19,7 +19,6 @@ def write_file_headers(datawriter):
 
 def register_into_csv(gatherer: Gatherer, datawriter):
     data = gatherer.collect()
-    print(data)
     datawriter.writerow([
         datetime.now().isoformat(),
         data['pressure'],

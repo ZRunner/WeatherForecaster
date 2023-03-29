c1775451dcbeffe2891cf67b8517b7869c8a7643
ZRunner <z.runner.mc@gmail.com>
2023-03-29 00:19:24 -0400

    add vps post

--- start-arima-predict.py
+++ start-arima-predict.py
@@ -1,5 +1,8 @@
+from requests import post
+
 from arima_methods import load_model, get_sensors_dataset, predict
 
+URL = "https://wf.zrunner.me/data"
 
 def forecast(hours: int):
     n_periods = 4 * hours
@@ -7,7 +10,10 @@ def forecast(hours: int):
     data = get_sensors_dataset().resample('15min').mean()[-n_periods:]
     data["hour"] = data.index.hour
     predictions = predict(model, data, n_periods=n_periods)
-    print(predictions)
+    json_obj = {}
+    for index, row in predictions.iterrows():
+        json_obj[index.isoformat(timespec='seconds')] = row["Prediction"]
+    post(URL, json=json_obj).raise_for_status()
 
 if __name__ == "__main__":
     forecast(hours=2)
